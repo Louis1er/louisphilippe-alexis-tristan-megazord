b533ccb8c43266a67b0fe5d9bfee3d0d3fc2be01
alxi21 <70409941+alxi21@users.noreply.github.com>
2021-03-22 17:23:50 -0400

    ui

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -3,3 +3,25 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  let section = document.querySelectorAll('.section-index');
+  
+  section.forEach(section => {
+    const image = section.querySelector('.image');
+    const title = section.querySelector('.title');
+    const year = section.querySelector('.year');
+    
+    gsap.timeline({
+      scrollTrigger: {
+        markers: true,
+        start: 'bottom bottom',
+        trigger: banner,
+        toggleActions: 'play none none none',
+      }
+    })
+    .from(image, {scale: 2})
+    .from(title, {y: 100, opacity: 0})
+    .from(year, {opacity: 0})
+  });
