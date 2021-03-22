bc24c19a203e3b930c1e9ca42252e644a199dcc7
Louis1er <lpgravel100@gmail.com>
2021-03-22 16:27:06 -0400

    section-animation2

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,23 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 gsap.registerPlugin(ScrollTrigger);
 
-  const sectionslist = document.querySelectorAll("section");
-  sectionslist.forEach((section) => { 
-  const image = section.querySelector(".album1");
-  const ol = section.querySelector("ol");
-  const li = section.querySelector("li");
-                    
+const sectionList = document.querySelectorAll(".section-li");
+  sectionList.forEach((section) => { 
+  const swiper= section.querySelector(".album1");
+  const title = section.querySelector(".title");
+
   gsap.timeline({
     scrollTrigger: section,
     markers: true,
-    start: 'bottom bottom',
-    trigger: section,
-    
+    toggleActions: 'reset complete reverse',
   })
-  .from(image, {
+  .from(swiper, {
     scale: 2,
     duration: 1,
   })
-  .from(ol, {
+  .from(title, {
     opacity: 0,
-    y: '100px',
-  })
-  .from(li, {
-    opacity: 0,
+    x: '100px',
   })
 })
-
-GSDevTools.create();
