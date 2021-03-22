7934fcbcf7303322d6b82d4efc2ba64a335b6065
Louis1er <lpgravel100@gmail.com>
2021-03-22 18:03:17 -0400

    anim_disco

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -8,5 +8,39 @@ const swiper = new Swiper('.swiper-container', {
   })
 
   
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionList = document.querySelectorAll(".section-li");
+  sectionList.forEach((banner) => { 
+  const animation= banner.querySelector(".anim_scroll");
+  const animation_left= banner.querySelector(".anim_scroll_left");
+  
+  
+
+  gsap.timeline({
+    scrollTrigger:  {
+      trigger: banner, 
+      markers: true,
+      toggleActions: 'play complete reverse reset',
+     
+    }
+   
+  })
+
+
+  .from(animation, {
+    x: '200%',
+    duration: 3, 
+  })
+
+  .from(animation_left, {
+    x: '-110%',
+    duration: 3, 
+  }, '<' );
+
+ 
+  
+
+})
 
   
