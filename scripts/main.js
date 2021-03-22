811c30248421a1216fe6b2c43505b5f7f7958ce0
Louis1er <lpgravel100@gmail.com>
2021-03-22 17:05:02 -0400

    animalo

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,36 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const sectionList = document.querySelectorAll(".section-li");
-  sectionList.forEach((section) => { 
-  const swiper= section.querySelector(".album1");
-  const title = section.querySelector(".title");
+  sectionList.forEach((banner) => { 
+  const image1= banner.querySelector(".album1");
+  const image2= banner.querySelector(".album2");
+  const title = banner.querySelector(".title");
 
   gsap.timeline({
-    scrollTrigger: section,
-    markers: true,
-    toggleActions: 'reset complete reverse',
+    scrollTrigger:  {
+      trigger: image1, 
+      trigger: image2, 
+      markers: true,
+      toggleActions: "play play none none"
+    }
+   
   })
-  .from(swiper, {
-    scale: 2,
-    duration: 1,
+
+
+  .from(image1, {
+    x: '200%',
+    duration: 3,
   })
+
+  .from(image2,{
+    x: '-100%',
+    duration: 2,
+  })
+
+  
+  
+
+
   .from(title, {
     opacity: 0,
     x: '100px',
