627e0fe7618a719f20728b323b1a76b29082de52
Tristan Girard <70409109+boub229@users.noreply.github.com>
2021-03-23 13:52:12 -0400

    sadsdad

--- scripts/palmares.js
+++ scripts/palmares.js
@@ -1,57 +1,70 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      992: {
-        slidesPerView: 2,
-      }
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
     }
-  });
+  }
+});
 
-  gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-const sectionList = document.querySelectorAll("section");
-  sectionList.forEach((banner) => { 
-  const animation= banner.querySelector(".card");
-  const animation_left= banner.querySelector(".anim_right_p");
-  
-  
 
+
+const sectionNom = document.querySelectorAll('section');
+sectionNom.forEach((banner) => {
+  const Nom = banner.querySelector('h1');
+  const swiper = banner.querySelector('.swiper-container');
+  const vid = banner.querySelector('.video');
   gsap.timeline({
-    scrollTrigger:  {
-      trigger: '.card', 
+    scrollTrigger: {
+      trigger: banner,
       markers: true,
       toggleActions: 'play complete reverse reset',
-     
     }
-   
   })
-
-
-  .from(animation, {
-    x: '200%',
-    duration: 2, 
-    opacity: 0,
-  })
-
-  .from(animation_left, {
-    x: '-110%',
-    duration: 2, 
-    opacity: 0,
-  }, '<' );
-
- 
-  
-
+    .from(Nom, {
+      x: '200%',
+      duration: 2,
+      opacity: 0,
+    })
+    .from(swiper, {
+      x: '200%',
+      duration: 1,
+      opacity: 0,
+    })
+    .from(vid, {
+      x: '200%',
+      duration: 2,
+      opacity: 0,
+    }, '<' );
 })
+
+const sectionList = document.querySelectorAll(".card");
+sectionList.forEach((banner) => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: banner,
+      markers: true,
+      toggleActions: 'play complete reverse reset',
+    }
+  })
+    .from(banner, {
+      x: '200%',
+      duration: 2,
+      opacity: 0,
+    }, '<' );
+})
+
